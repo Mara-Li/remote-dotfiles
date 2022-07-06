01db440c3d9948e96e77b1a7daebf1d8706076bc
Mara <mara-li@outlook.fr>
2022-07-06 17:50:32 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,11 @@ sudo apt-get install -y mesa-common-dev libx11-dev r-cran-rgl r-cran-rglpk r-cra
 sudo apt-get install -y r-cran-reshape r-cran-reshape2 r-cran-rmysql
 conda config --add channels conda-forge
 conda init bash
+conda init fish
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
+sudo apt-get install -y wget apt-transport-https software-properties-common
+wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y powershell
